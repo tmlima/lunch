c160dcb18fcb2d49285b36a27a0f98f6f78a2da2
tmlima <thiago.machado.lima@gmail.com>
2020-10-02 19:01:42 -0300

    specflow features moved to BDD project

---
+++ Lunch/Lunch.Test.BDD/LunchDbContextInMemory.cs
@@ -0,0 +1,10 @@
+﻿using Lunch.Infra.Data.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lunch.Test.BDD
+{
+    class LunchDbContextInMemory : LunchDbContext
+    {
+        public LunchDbContextInMemory( DbContextOptions<LunchDbContext> dbContextOptions ) : base( dbContextOptions ) { }
+    }
+}
