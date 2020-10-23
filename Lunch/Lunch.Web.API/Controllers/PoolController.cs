4ce17c5dcc396719639c2cb7e96e86855cdb667b
tmlima <thiago.machado.lima@gmail.com>
2020-10-23 17:42:52 -0300

    adding api project

---
+++ Lunch/Lunch.Web.API/Controllers/PoolController.cs
@@ -0,0 +1,40 @@
+﻿using Lunch.Domain.Entities;
+using Lunch.Domain.Interfaces;
+using Lunch.Web.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Lunch.Web.API.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class PoolController : ControllerBase
+    {
+        IPoolService poolService;
+
+        //public PoolController()
+        public PoolController( IPoolService poolService )
+        {
+            this.poolService = poolService;
+        }
+
+        // GET: api/<PoolController>
+        [HttpGet]
+        public IList<PoolReportModel> Get()
+        {
+            //IEnumerable<Pool> pools = poolService.GetAllAsync().Result;
+            IList<PoolReportModel> poolReports = new List<PoolReportModel>();
+            //foreach (Pool p in pools)
+            //{
+            //    poolReports.Add( new PoolReportModel()
+            //    {
+            //        Id = p.Id,
+            //        ClosingTime = p.ClosingTime,
+            //        RestaurantElected = p.RestaurantElected.Name
+            //    } );
+            //}
+
+            return poolReports;
+        }
+    }
+}
---
+++ Lunch/Lunch.Web.API/Models/PoolReportModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lunch.Web.API.Models
+{
+    public class PoolReportModel
+    {
+        public int Id { get; set; }
+        public DateTime ClosingTime { get; set; }
+        public string RestaurantElected { get; set; }
+
+    }
+}
